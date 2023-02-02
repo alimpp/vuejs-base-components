89e4e4e104d01230e6e232283c40677977694ac4
alimpp <hmdmpp@gmail.com>
2023-02-02 04:11:04 -0500

    Make disabled

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
 <template>
   <div class="app px-5 pt-2">
-    <h6>Normal Buttons</h6>
-    <div class="d-flex">
-      <baseButton color="primary" name="Hello World...!" :loading="false"/>
-      <baseButton class="mx-1" color="secondary" name="Hello World...!" :loading="false"/>
-      <baseButton class="mx-1" color="success" name="Hello World...!" :loading="false"/>
-      <baseButton class="mx-1" color="danger" name="Hello World...!" :loading="false"/>
-      <baseButton class="mx-1" color="warning" name="Hello World...!" :loading="false"/>
-      <baseButton class="mx-1" color="info" name="Hello World...!" :loading="false"/>
-      <baseButton class="mx-1" color="light" name="Hello World...!" :loading="false"/>
-      <baseButton class="mx-1" color="dark" name="Hello World...!" :loading="false"/>
-    </div>
-    <h6 class="pt-5">Loading Buttons</h6>
-    <div class="d-flex">
-      <baseButton color="primary" name="Hello World...!" :loading="true"/>
-      <baseButton class="mx-1" color="secondary" name="Hello World...!" :loading="true"/>
-      <baseButton class="mx-1" color="success" name="Hello World...!" :loading="true"/>
-      <baseButton class="mx-1" color="danger" name="Hello World...!" :loading="true"/>
-      <baseButton class="mx-1" color="warning" name="Hello World...!" :loading="true"/>
-      <baseButton class="mx-1" color="info" name="Hello World...!" :loading="true"/>
-      <baseButton class="mx-1" color="light" name="Hello World...!" :loading="true"/>
-      <baseButton class="mx-1" color="dark" name="Hello World...!" :loading="true"/>
-    </div>
-  </div>  
+    <index />
+  </div>
 </template>
 
 <script setup>
-import baseButton from '@/components/baseButton'
+import index from "@/components/buttons/index";
 </script>
 
 <style lang="scss" scoped>
-.app{
+.app {
   width: 100%;
   font-family: sans-serif;
   display: flex;
---
+++ src/components/base/baseButton.vue
@@ -0,0 +1,159 @@
+<template>
+  <button
+    class="button"
+    :class="{
+      primary_background: primaryBackground,
+      secondary_background: secondaryBackground,
+      success_background: successBackground,
+      danger_background: dangerBackground,
+      warning_background: warningBackground,
+      info_background: infoBackground,
+      light_background: lightBackground,
+      dark_background: darkBackground,
+      disbaled_button: disabledButton,
+    }"
+  >
+    <div class="" v-if="loading">
+      <div class="spinner-border spinner-border-sm" role="status"></div>
+      <span class="px-1">Loading</span>
+    </div>
+
+    <div v-else>
+      <i
+        class="bi"
+        :class="{
+          'bi-person-fill': userIcon,
+          'bi-envelope-at': emailIcon,
+          'bi-send-check': sendIcon,
+          'bi-chat-dots': chatIcon,
+        }"
+      ></i>
+      {{ name }}
+    </div>
+  </button>
+</template>
+
+<script setup>
+import { computed, ref, onMounted } from "vue";
+
+const primaryBackground = computed(() => {
+  if (props.color === "primary") {
+    return true;
+  }
+});
+
+const secondaryBackground = computed(() => {
+  if (props.color === "secondary") {
+    return true;
+  }
+});
+
+const successBackground = computed(() => {
+  if (props.color === "success") {
+    return true;
+  }
+});
+
+const dangerBackground = computed(() => {
+  if (props.color === "danger") {
+    return true;
+  }
+});
+
+const warningBackground = computed(() => {
+  if (props.color === "warning") {
+    return true;
+  }
+});
+
+const infoBackground = computed(() => {
+  if (props.color === "info") {
+    return true;
+  }
+});
+
+const lightBackground = computed(() => {
+  if (props.color === "light") {
+    return true;
+  }
+});
+
+const darkBackground = computed(() => {
+  if (props.color === "dark") {
+    return true;
+  }
+});
+
+const userIcon = computed(() => {
+  if (props.icon === "user") {
+    return true;
+  }
+});
+
+const emailIcon = computed(() => {
+  if (props.icon === "email") {
+    return true;
+  }
+});
+
+const sendIcon = computed(() => {
+  if (props.icon === "send") {
+    return true;
+  }
+});
+
+const chatIcon = computed(() => {
+  if (props.icon === "chat") {
+    return true;
+  }
+});
+
+const disabledButton = computed(() => {
+  if (props.disable == true) {
+    return true;
+  }
+});
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  name: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  icon: {
+    type: String,
+    default: "",
+    required: false,
+  },
+  disable: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.button {
+  padding: 5px;
+  border: none;
+  border-radius: 5px;
+}
+.disbaled_button {
+  background: rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(0px);
+  -webkit-backdrop-filter: blur(0px);
+  cursor: not-allowed;
+  pointer-events: none;
+}
+</style>
